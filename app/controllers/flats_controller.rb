81833878b2c330d89ceff8ddfbf898bc97a93cb5
Boris Knezevic <boris.knezevic0@gmail.com>
2023-02-20 17:57:13 +0100

    add index/show for flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+
+class FlatsController < ApplicationController
+  before_action :set_flats
+  URL = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+
+
+  def index
+  end
+
+  def show
+    id = params[:id]
+    @flat = @flats.find { |flat| flat["id"] == id.to_i }
+  end
+  
+  private
+
+  def set_flats
+    @flats = JSON.parse(URI.open(URL).read)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "flats", to: "flats#index"
+  get "flats/:id", to: "flats#show", as: :flat
 end
